d5b4298caddaace9ef2fef1ed28a47274c676606
Becca Rose <hello@beccarose.co.uk>
2020-02-28 12:05:36 +0000

    .

--- sketch.js
+++ sketch.js
@@ -7,29 +7,29 @@ var imgH;
 
 
 function preload() {
-  // load images
-  for (var i = 0; i < 35; i++) {
-    img[i] = loadImage("imgs/" + i + ".png");
-    print(i);
-  }
+    // load images
+    for (var i = 0; i < 35; i++) {
+        img[i] = loadImage("imgs/" + i + ".png");
+        print(i);
+    }
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-   canvas.position(0,0);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
     canvas.style("z-index", "-1");
-  background(255, 255);
-  noLoop();
-  imageMode(CENTER);
-  noStroke();
-  imageSnap();
-    
-    
+    background(255, 255);
+    noLoop();
+    imageMode(CENTER);
+    noStroke();
+    imageSnap();
+
+
 }
 
-function windowResized(){
+function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-      imageSnap();
+    imageSnap();
 }
 
 /////////////////////////////
@@ -37,56 +37,56 @@ function windowResized(){
 /////////////////////////////
 
 function imageSnap() {
-  
-  background(248, 242, 201);
-  //image randoms
-  var rand1 = ~~random(35);
-  var rand2 = ~~random(35);
-  var rand3 = ~~random(35);
-    
+
+    background(248, 242, 201);
+    //image randoms
+    var rand1 = ~~random(35);
+    var rand2 = ~~random(35);
+    var rand3 = ~~random(35);
 
 
 
-  //display images
 
-push(); // Start a new drawing state
-translate((width / 2) - 300, (height / 2));
-rotate(rand2);
-  tint(0, 153, 204, ~~random(100, 200)); // Apply transparency without changing color
-  image(img[rand3], 0, 0, imgW, imgH);
+    //display images
+
+    push(); // Start a new drawing state
+    translate((width / 2) - 300, (height / 2));
+    rotate(rand2);
+    tint(0, 153, 204, ~~random(100, 200)); // Apply transparency without changing color
+    image(img[rand3], 0, 0, imgW, imgH);
     pop(); // Restore original state
-    
-  push(); // Start a new drawing state
-translate(width / 2, (height / 2) - 10);
-rotate(rand3);
-    tint(255, 255, 0, ~~random(100, 200)); // Apply transparency without changing color
-  image(img[rand2], 0, 0, imgW, imgH);
- 
-       pop(); // Restore original state
-    
-  push(); // Start a new drawing state
-translate((width / 2) + 300, (height / 2) + 30);
-rotate(rand1);
-    tint(7, 221, 153, ~~random(100, 200)); // Apply transparency without changing color
-  image(img[rand1],0 , 0, imgW, imgH);
 
-       pop(); // Restore original state
-  
+    push(); // Start a new drawing state
+    translate(width / 2, (height / 2) - 10);
+    rotate(rand3);
+    tint(255, 255, 0, ~~random(100, 200)); // Apply transparency without changing color
+    image(img[rand2], 0, 0, imgW, imgH);
+
+    pop(); // Restore original state
+
+    push(); // Start a new drawing state
+    translate((width / 2) + 300, (height / 2) + 30);
+    rotate(rand1);
+    tint(7, 221, 153, ~~random(100, 200)); // Apply transparency without changing color
+    image(img[rand1], 0, 0, imgW, imgH);
+
+    pop(); // Restore original state
+
 }
 
 
- // CHANGE WEBSITE TITLE DYNAMICALLY WITH EMOJI
- 
+// CHANGE WEBSITE TITLE DYNAMICALLY WITH EMOJI
+
 const EMOJIS = [
   'Control(╯°□°)╯︵┻━┻ Shift',
   'Control(ヘ･_･)ヘ ┳━┳ ',
-    
+
 ]
 setInterval(() => {
-  document.title = randomEmoji();
+    document.title = randomEmoji();
 }, 2000);
 
-function randomEmoji(){
-  let randIndex = Math.floor(Math.random() * EMOJIS.length);
-  return EMOJIS[randIndex];
+function randomEmoji() {
+    let randIndex = Math.floor(Math.random() * EMOJIS.length);
+    return EMOJIS[randIndex];
 }
